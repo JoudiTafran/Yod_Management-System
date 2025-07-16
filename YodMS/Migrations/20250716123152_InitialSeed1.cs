22af5f4f8588b9a5ddc6e7e591ce644d3e5c553e
Joudi <jouditafran271@gamil.com>
2025-07-16 15:38:06 +0300

    Seeding for Roles - DocumentTypes - RoleDocPerms

---
+++ YodMS/Migrations/20250716123152_InitialSeed1.cs
@@ -0,0 +1,287 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace YodMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSeed1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "DocumentTypes",
+                columns: new[] { "DocTypeId", "TypeName" },
+                values: new object[,]
+                {
+                    { 1, "تصور" },
+                    { 2, "تقرير" },
+                    { 3, "بيان" },
+                    { 4, "محضر" },
+                    { 5, "خطة" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "RoleId", "RoleName" },
+                values: new object[,]
+                {
+                    { 1, "رئيس الهيئة التنفيذية" },
+                    { 2, "نائب الرئيس" },
+                    { 3, "أمين عام" },
+                    { 4, "مسؤول مالي" },
+                    { 5, "مسؤول علاقات" },
+                    { 6, "مسؤول أكاديمي" },
+                    { 7, "مسؤول أنشطة" },
+                    { 8, "مسؤولة طالبات" },
+                    { 9, "مسؤول إعلامي" },
+                    { 10, "مٌراقب" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "RoleDocPerms",
+                columns: new[] { "DocTypeId", "RoleId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 2, 1 },
+                    { 3, 1 },
+                    { 1, 2 },
+                    { 2, 2 },
+                    { 1, 3 },
+                    { 2, 3 },
+                    { 3, 3 },
+                    { 4, 3 },
+                    { 1, 4 },
+                    { 2, 4 },
+                    { 1, 5 },
+                    { 2, 5 },
+                    { 5, 5 },
+                    { 1, 6 },
+                    { 2, 6 },
+                    { 5, 6 },
+                    { 1, 7 },
+                    { 2, 7 },
+                    { 5, 7 },
+                    { 1, 8 },
+                    { 2, 8 },
+                    { 5, 8 },
+                    { 1, 9 },
+                    { 2, 9 },
+                    { 5, 9 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 2, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 3, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 1, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 2, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 1, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 2, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 3, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 4, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 1, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 2, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 1, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 2, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 5, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 1, 6 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 2, 6 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 5, 6 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 1, 7 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 2, 7 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 5, 7 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 1, 8 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 2, 8 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 5, 8 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 1, 9 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 2, 9 });
+
+            migrationBuilder.DeleteData(
+                table: "RoleDocPerms",
+                keyColumns: new[] { "DocTypeId", "RoleId" },
+                keyValues: new object[] { 5, 9 });
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "DocumentTypes",
+                keyColumn: "DocTypeId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "DocumentTypes",
+                keyColumn: "DocTypeId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "DocumentTypes",
+                keyColumn: "DocTypeId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "DocumentTypes",
+                keyColumn: "DocTypeId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "DocumentTypes",
+                keyColumn: "DocTypeId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: 9);
+        }
+    }
+}
--- YodMS/Models/DataBase_Manager/AppDbContext.cs
+++ YodMS/Models/DataBase_Manager/AppDbContext.cs
@@ -18,9 +18,12 @@ namespace YodMS.Models.DataBase_Manager
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // مفتاح مركب لـ RoleDocPerm
+            base.OnModelCreating(modelBuilder);
+
+            // Define Keys:
+
             modelBuilder.Entity<RoleDocPerms>()
-                .HasKey(rp => new { rp.RoleId, rp.DocTypeId });
+                .HasKey(rp => new { rp.RoleId, rp.DocTypeId }); // مفتاح مركب 
 
             modelBuilder.Entity<DocumentTypes>().HasKey(dt => dt.DocTypeId);
             modelBuilder.Entity<Users>().HasKey(u => u.UserId);
@@ -31,6 +34,7 @@ namespace YodMS.Models.DataBase_Manager
             modelBuilder.Entity<Votes>().HasKey(v => v.VoteId);
             modelBuilder.Entity<VoteSessions>().HasKey(vs => vs.VoteSessionId);
 
+            // Define relationships:
 
             modelBuilder.Entity<RoleDocPerms>()
                 .HasOne(rp => rp.Role)
@@ -42,7 +46,6 @@ namespace YodMS.Models.DataBase_Manager
                 .WithMany(d => d.RoleDocPerms)
                 .HasForeignKey(rp => rp.DocTypeId);
 
-            // علاقة Document ↔ OwnerUser
             modelBuilder.Entity<Documents>()
                 .HasOne(d => d.OwnerUser)
                 .WithMany(u => u.Documents)
@@ -53,7 +56,6 @@ namespace YodMS.Models.DataBase_Manager
                 .WithMany(dt => dt.Documents)
                 .HasForeignKey(d => d.DocTypeId);
 
-            // علاقة Review ↔ Document
             modelBuilder.Entity<Reviews>()
                 .HasOne(r => r.Document)
                 .WithMany(d => d.Reviews)
@@ -81,6 +83,58 @@ namespace YodMS.Models.DataBase_Manager
                 .HasOne(v => v.CreatedByUser)
                 .WithMany(u => u.VoteSessionsCreated)
                 .HasForeignKey(v => v.CreatedByUserId);
+
+            // Filling out the models:
+
+            modelBuilder.Entity<Roles>().HasData(
+                new Roles { RoleId = 1, RoleName = "رئيس الهيئة التنفيذية" },
+                new Roles { RoleId = 2, RoleName = "نائب الرئيس" },
+                new Roles { RoleId = 3, RoleName = "أمين عام" },
+                new Roles { RoleId = 4, RoleName = "مسؤول مالي" },
+                new Roles { RoleId = 5, RoleName = "مسؤول علاقات" },
+                new Roles { RoleId = 6, RoleName = "مسؤول أكاديمي" },
+                new Roles { RoleId = 7, RoleName = "مسؤول أنشطة" },
+                new Roles { RoleId = 8, RoleName = "مسؤولة طالبات" },
+                new Roles { RoleId = 9, RoleName = "مسؤول إعلامي" },
+                new Roles { RoleId = 10, RoleName = "مٌراقب" }
+             );
+
+            modelBuilder.Entity<DocumentTypes>().HasData(
+                new DocumentTypes { DocTypeId = 1, TypeName = "تصور" },
+                new DocumentTypes { DocTypeId = 2, TypeName = "تقرير" },
+                new DocumentTypes { DocTypeId = 3, TypeName = "بيان" },
+                new DocumentTypes { DocTypeId = 4, TypeName = "محضر" },
+                new DocumentTypes { DocTypeId = 5, TypeName = "خطة" }
+             );
+
+            modelBuilder.Entity<RoleDocPerms>().HasData(
+                new RoleDocPerms { RoleId = 1, DocTypeId = 1 },
+                new RoleDocPerms { RoleId = 1, DocTypeId = 2 },
+                new RoleDocPerms { RoleId = 1, DocTypeId = 3 },
+                new RoleDocPerms { RoleId = 2, DocTypeId = 1 },
+                new RoleDocPerms { RoleId = 2, DocTypeId = 2 },
+                new RoleDocPerms { RoleId = 3, DocTypeId = 1 },
+                new RoleDocPerms { RoleId = 3, DocTypeId = 2 },
+                new RoleDocPerms { RoleId = 3, DocTypeId = 3 },
+                new RoleDocPerms { RoleId = 3, DocTypeId = 4 },
+                new RoleDocPerms { RoleId = 4, DocTypeId = 1 },
+                new RoleDocPerms { RoleId = 4, DocTypeId = 2 },
+                new RoleDocPerms { RoleId = 5, DocTypeId = 1 },
+                new RoleDocPerms { RoleId = 5, DocTypeId = 2 },
+                new RoleDocPerms { RoleId = 5, DocTypeId = 5 },
+                new RoleDocPerms { RoleId = 6, DocTypeId = 1 },
+                new RoleDocPerms { RoleId = 6, DocTypeId = 2 },
+                new RoleDocPerms { RoleId = 6, DocTypeId = 5 },
+                new RoleDocPerms { RoleId = 7, DocTypeId = 1 },
+                new RoleDocPerms { RoleId = 7, DocTypeId = 2 },
+                new RoleDocPerms { RoleId = 7, DocTypeId = 5 },
+                new RoleDocPerms { RoleId = 8, DocTypeId = 1 },
+                new RoleDocPerms { RoleId = 8, DocTypeId = 2 },
+                new RoleDocPerms { RoleId = 8, DocTypeId = 5 },
+                new RoleDocPerms { RoleId = 9, DocTypeId = 1 },
+                new RoleDocPerms { RoleId = 9, DocTypeId = 2 },
+                new RoleDocPerms { RoleId = 9, DocTypeId = 5 }
+             );
         }
     }
 }
